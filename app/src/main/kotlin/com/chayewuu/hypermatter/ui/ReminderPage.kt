d53e7bfbf1d91c78bd8112307b4e2322de4f1b12
ChaYeWuu <2409507103@qq.com>
2026-09-03 12:54:11 +0800

    chore: 清理 ReminderPage 未使用 import

--- app/src/main/kotlin/com/chayewuu/hypermatter/ui/ReminderPage.kt
+++ app/src/main/kotlin/com/chayewuu/hypermatter/ui/ReminderPage.kt
@@ -10,7 +10,6 @@ import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
